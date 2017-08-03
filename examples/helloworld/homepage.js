7b87ca9acfa2cb22938a8488cd0c241c872c19af
Dave Winer <dave.winer@gmail.com>
2017-08-03 15:11:24 -0400

    v0.4.16

--- examples/helloworld/homepage.js
+++ examples/helloworld/homepage.js
@@ -19,21 +19,13 @@ function startup () {
 		};
 	shell.init (options, function (appPrefsFromStorage) {
 		initTwitterMenuItems ();
+		self.setInterval (everySecond, 1000); 
 		if (twIsTwitterConnected ()) {
 			twGetUserInfo (twGetScreenName (), function (userinfo) {
-				twUserInfo = userinfo;
+				console.log ("startup: userinfo == " + jsonStringify (userinfo));
 				$("#idUserName").text (userinfo.name);
 				showHelloWorldMessage ();
-				console.log ("startup: twUserInfo == " + jsonStringify (twUserInfo));
-				twGetTwitterConfig (function () { 
-					everySecond (); //don't wait for the next second, call immediately
-					self.setInterval (everySecond, 1000); 
-					});
 				});
-			}
-		else {
-			everySecond (); //don't wait for the next second, call immediately
-			self.setInterval (everySecond, 1000); 
 			}
 		});
 	}
